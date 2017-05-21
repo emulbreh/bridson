0f144d01db2083b4e57ed3348503a894450f2a53
Johannes Dollinger <emulbreh@googlemail.com>
2017-05-21 12:38:26 +0200

    Switch to MIT License

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ setup(
     url='http://github.com/emulbreh/bridson/',
     version='0.1.0',
     packages=find_packages(),
-    license=u'BSD License',
+    license=u'MIT License',
     author=u'Johannes Dollinger',
     description=u'poisson disc sampling of 2-dimensional sample domain',
     long_description=description,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3'
     ]
